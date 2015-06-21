00f45fe518580800a8206aa7c48f9f6551c18dad
jdesilvio <john.desilvio@yahoo.com>
2015-06-20 23:10:46 -0400

    added form it input card name

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,5 +3,22 @@ class CardsController < ApplicationController
   end
 
   def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      render :index
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:idea).permit(:card_name)
   end
 end
