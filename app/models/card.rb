41f66af37a31f2f197b51ac5cb70d75643aba83e
jdesilvio <john.desilvio@yahoo.com>
2015-06-28 14:27:33 -0400

    cloned from pgConnect repo; udated database.yml and db variables

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,12 +4,12 @@ end
 class Pgdb < ActiveRecord::Base
 
   self.abstract_class = true
-  self.table_name = 'testtable'
+  self.table_name = 'psa'
 
   establish_connection :pg_development
 
   def self.getCardInfo(card_name)
-    get = connection.query("SELECT * FROM testtable WHERE test=#{connection.quote(card_name)}")
+    get = connection.query("SELECT * FROM psa WHERE card=#{connection.quote(card_name)}")
     get
   end
 end
