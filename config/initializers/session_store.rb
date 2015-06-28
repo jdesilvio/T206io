1937dcd17be93cdebb1811210e7fb2c86970b486
jdesilvio <john.desilvio@yahoo.com>
2015-06-28 14:49:25 -0400

    changed all pgConnect references to monster

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pgConnect_session'
+Rails.application.config.session_store :cookie_store, key: '_monster_session'
