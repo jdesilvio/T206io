0d2ff81b482f316505f5a9478adb61b801d46430
jdesilvio <john.desilvio@yahoo.com>
2015-07-01 22:22:55 -0400

    added capistrano task to deploy database.yml

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -65,6 +65,6 @@ after "deploy:update_code","deploy:config_symlink"
 namespace :deploy do
 
   task :config_symlink do
-    run "cp #{shared_path}/../../shared/database.yml #{release_path}/config/database.yml"
+    run "cp #{shared_path}/shared/database.yml #{release_path}/config/database.yml"
   end
 end
