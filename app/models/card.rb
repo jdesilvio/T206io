babdceebac856fdb74e4bc90afc34072edb872d1
jdesilvio <john.desilvio@yahoo.com>
2015-07-02 21:45:21 -0400

    added heroku config vars

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,13 @@
 class Card < ActiveRecord::Base
 end
 
-class Pgdb < ActiveRecord::Base
+class Pgdb < ActiveRecord::Base.establish_connection(
+    adapter: => ENV['PG_ADAPTER'],
+    database: => ENV['PG_DB'],
+    username: => ENV['PG_USER'],
+    password: => ENV['PG_PW'],
+    host: => ENV['PG_HOST']
+  )
 
   self.abstract_class = true
   self.table_name = 'psa'
