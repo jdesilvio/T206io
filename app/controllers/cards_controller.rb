6c877ed4c955c0af16deda2061f3441405003345
jdesilvio <john.desilvio@yahoo.com>
2015-08-29 00:53:37 -0400

    added grade and back summaries

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,7 +11,8 @@ class CardsController < ApplicationController
 
     if @card.save
       @info = Psa_pop.where("psa_card LIKE ? AND psa_amount > 0", "%#{card_params[:card_name]}%".upcase).order(:full_back, :psa_grade_name)
-    @agg_backs = Pgdb.connection.execute("select agg_backs('#{card_params[:card_name]}')")
+      @summary_backs = Pgdb.connection.execute("SELECT * FROM summary_backs('#{card_params[:card_name]}')")
+      @summary_grades = Pgdb.connection.execute("SELECT * FROM summary_grades('#{card_params[:card_name]}')")
       render :index
     else
       render 'new'
