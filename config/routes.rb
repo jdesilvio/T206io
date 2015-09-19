2a3a18e5c406bb2f26d676d86a8c98c0967a6c3b
jdesilvio <john.desilvio@yahoo.com>
2015-09-19 16:44:41 -0400

    Added user authentication links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :cards
   root :to => 'cards#new'
 
+  unauthenticated do
+    root to: 'cards#new', :as => 'unauthenticated'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
