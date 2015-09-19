f98d21d190b0d5339d9471a4da5f26238c42d3a2
jdesilvio <john.desilvio@yahoo.com>
2015-09-19 11:42:51 -0400

    Finished Devise and mailer config and generated Devise views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise configurations
+  config.action_mailer.default_url_options = { host: 't206.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
