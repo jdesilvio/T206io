1fce0376c9dba9f695ccb7c3e8afa22f75ae715c
jdesilvio <john.desilvio@yahoo.com>
2015-11-14 14:59:06 -0500

    Added dynamic body id tag helper and updated associated CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def page_id
+    if id = content_for(:body_id) and id.present?
+      return id
+    else
+      base = controller.class.to_s.gsub("Controller", '').underscore.gsub("/", '_')
+      return "#{base}-#{controller.action_name}"
+    end
+  end
+
 end
