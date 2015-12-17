b654a677ee0b121e0d3cfaf7b3c34af8c8137135
John DeSilvio <john.desilvio@yahoo.com>
2015-12-17 11:15:55 -0500

    Fixed footer positioning, updated CSS for navbar

--- app/assets/javascripts/scroll_fade.js
+++ app/assets/javascripts/scroll_fade.js
@@ -3,6 +3,6 @@ var text = $(".content");
 $(window).on("scroll", function(e) {
   var text = $(".content");
   text.css('mask-image', '-webkit-linear-gradient(top, rgba(0,0,0,0) ' +
-  ($('body').scrollTop()+96) + 'px , rgba(0,0,0,1)' +
-  ($('body').scrollTop()+128) + 'px)');
+  ($('body').scrollTop()+128) + 'px , rgba(0,0,0,1)' +
+  ($('body').scrollTop()+160) + 'px)');
 })
