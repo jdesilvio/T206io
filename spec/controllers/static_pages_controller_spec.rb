05f0984913f8c5ee266d9a6254cc6935570b5ef5
John DeSilvio <john.desilvio@yahoo.com>
2015-12-17 14:29:46 -0500

    Added test for StaticPagesController

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+RSpec.describe StaticPagesController, :type => :controller do
+
+  # Index
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  # Landing
+  describe "GET #landing" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :landing
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the landing template" do
+      get :landing
+      expect(response).to render_template("landing")
+    end
+  end
+
+  # About
+  describe "GET #about" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :about
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+
+  # Contribute
+  describe "GET #contribute" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :contribute
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the contribute template" do
+      get :contribute
+      expect(response).to render_template("contribute")
+    end
+  end
+end
