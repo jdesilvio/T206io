d67db61495bbc180f9bc1a28e91266d793fb994b
John DeSilvio <john.desilvio@yahoo.com>
2015-12-21 20:19:49 -0500

    Added StaticPagesController test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,8 +2,22 @@ require "rails_helper"
 
 RSpec.describe StaticPagesController, :type => :controller do
 
-  # Index
-  describe "GET #index" do
+  # Index - when unauthenticated
+  describe "GET #index when unauthenticated - route to #landing" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :landing
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the landing template" do
+      get :landing
+      expect(response).to render_template("landing")
+    end
+  end
+
+  # Index - when authenticated
+  describe "GET #index when authenticated - route to #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
       expect(response).to be_success
@@ -11,7 +25,7 @@ RSpec.describe StaticPagesController, :type => :controller do
     end
 
     it "renders the index template" do
-      get :index
+      get :landing
       expect(response).to render_template("index")
     end
   end
