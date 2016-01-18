cc8aa23c64f10bf8b15266928ff3b72f6573fdf2
John DeSilvio <john.desilvio@yahoo.com>
2016-01-18 00:49:28 -0500

    Added flexbox layout for header and removed the scrolling fade into header

--- app/assets/javascripts/scroll_fade.js
+++ app/assets/javascripts/scroll_fade.js
@@ -1,8 +1,8 @@
-var text = $(".content");
+//var text = $(".content");
 
-$(window).on("scroll", function(e) {
-  var text = $(".content");
-  text.css('mask-image', '-webkit-linear-gradient(top, rgba(0,0,0,0) ' +
-  ($('body').scrollTop()+128) + 'px , rgba(0,0,0,1)' +
-  ($('body').scrollTop()+160) + 'px)');
-})
+//$(window).on("scroll", function(e) {
+//  var text = $(".content");
+//  text.css('mask-image', '-webkit-linear-gradient(top, rgba(0,0,0,0) ' +
+//  ($('body').scrollTop()+128) + 'px , rgba(0,0,0,1)' +
+//  ($('body').scrollTop()+160) + 'px)');
+//})
