69f7c2d61e47cdba831bfdb95044d38a8586b2bf
John DeSilvio <john.desilvio@yahoo.com>
2016-07-02 00:02:27 -0400

    Fixed bug in cards controller where an apostrophe in card name was causing a query error

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,9 +13,20 @@ class CardsController < ApplicationController
     @card = Card.new(card_params)
 
     if @card.save
-      @info = Psa_pop.where("psa_card LIKE ? AND psa_amount > 0", "%#{card_params[:card_name]}%".upcase).order(:full_back, :psa_grade_name)
-      @summary_backs = Pgdb.connection.execute("SELECT * FROM summary_backs('#{card_params[:card_name]}')")
-      @summary_grades = Pgdb.connection.execute("SELECT * FROM summary_grades('#{card_params[:card_name]}')")
+      # Query for full population report for card
+      @info = Psa_pop.where("psa_card LIKE ? AND psa_amount > 0",
+                            "%#{card_params[:card_name]}%".upcase).order(:full_back,
+                            :psa_grade_name)
+      
+      # Query for back summary info for card
+      @summary_backs = Pgdb.connection.execute(
+          "SELECT * FROM summary_backs(#{ActiveRecord::Base.connection.quote(card_params[:card_name])})"
+      )
+      
+      # Query for grade summary info for card
+      @summary_grades = Pgdb.connection.execute(
+          "SELECT * FROM summary_grades(#{ActiveRecord::Base.connection.quote(card_params[:card_name])})"
+      )
       render :index
     else
       render 'new'
